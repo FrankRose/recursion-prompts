eb7762dd31d7f74dfc7b8ebe46c8d222ea039334
FrankRose <franklin.rose@gmail.com>
2018-05-30 07:37:00 -0400

    Completed #15

--- src/recursion.js
+++ src/recursion.js
@@ -206,6 +206,12 @@ var gcd = function(x, y) {
 // compareStr('house', 'houses') // false
 // compareStr('tomato', 'tomato') // true
 var compareStr = function(str1, str2) {
+    if (str1.length === 0 || str2.length === 0) {
+        return str1.length === str2.length;
+    }
+
+    return str1[0].toLowerCase() === str2[0].toLowerCase()
+        && compareStr(str1.slice(1), str2.slice(1));
 };
 
 // 16. Write a function that accepts a string and creates an array where each letter
