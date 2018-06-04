104c5f35457b16a565b44900f27cf3e0dafbf0db
FrankRose <franklin.rose@gmail.com>
2018-06-04 19:12:41 -0400

    Completed #32 - Eliminate consecutive duplicates

--- src/recursion.js
+++ src/recursion.js
@@ -539,7 +539,20 @@ var letterTally = function(str, obj) {
 // elements should not be changed.
 // compress([1,2,2,3,4,4,5,5,5]) // [1,2,3,4,5]
 // compress([1,2,2,3,4,4,2,5,5,5,4,4]) // [1,2,3,4,2,5,4]
-var compress = function(list) {};
+var compress = function(list) {
+  if (list.length < 2) {
+    return list;
+  }
+
+  let output = [];
+  let index = 1;
+  output.push(list[0]);
+  while (list[index] === list[0]) {
+    index++
+  }
+
+  return output.concat(compress(list.slice(index)));
+};
 
 // 33. Augument every element in a list with a new value where each element is an array
 // itself.
